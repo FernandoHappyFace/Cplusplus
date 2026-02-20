6cf3919c4188101d322b709ead3cafc8970c3844
FernandoHappyFace <nando.pugacano@gmail.com>
2026-02-20 09:41:14 -0600

    Add files via upload

---
+++ HolaMundo/Holamundo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;// se usa para evitar ponerlo en cada linea, clase de c++
+//Comentarios de una linea
+/* comentarios de varias lineas
+asi
+asi tambien*/
+void funcionx(){
+    
+    cout<<"No se";
+}
+void main(){
+    cout<<"Hola,Mundo!"<<endl;
+    funcionx();
+    
+}
